0b5b394ee58d48338561fe1cad042064e8271410
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-09-13 22:56:15 -0300

    agrego NavBar

---
+++ reactfolder/src/components/NavBar.js
@@ -0,0 +1,19 @@
+function navBar (){
+
+    return(
+        <div className="navBar">
+            <h2>
+                Mi E-comerce
+            </h2>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Servicios</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+          
+    );
+}
+export default navBar;
