5fd7cd4f04a1aff6590b26644185e07533100210
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-09-18 10:00:30 -0300

    cree el html del la card

---
+++ reactfolder/src/components/Product/Card.jsx
@@ -0,0 +1,58 @@
+function Card (punctuation, price, distance,guiaName,place){
+    let price = price; 
+    let punctuation= punctuation;
+    let distance= distance;
+    let = guiaName=guiaName;
+    let = place=place;
+
+    return(
+        <div className= 'card'>
+            <div className='card-title'>
+                <div 'card-title-guia'>
+                    <p>(guiaName)</p>
+                    <p>(place)</p>
+                </div>
+
+            </div>
+            <div className= 'card-img'>
+                <img src="" alt="" />
+            </div>
+
+            <div className='card-detail'>
+                <div ClasName='card-detail-Puntation'>
+                    <img src="" alt="" />
+                    <p>(punctuation)</p>
+                </div>
+                <div ClasName='card-detail-price'>
+                    <img src="" alt="" />
+                    <p>(price)</p>
+                </div>
+                <div ClasName='card-detail-distance'>
+                    <img src="" alt="" />
+                    <p>(distance)</p>
+                </div>
+                <div ClasName='card-detail-coments'>
+                    <img src="" alt="" />
+                    <p>(coments)</p>
+                </div>
+                <div ClasName='card-detail-time'>
+                    <img src="" alt="" />
+                    <p>(time)</p>
+                </div>
+
+            </div>
+
+            <div className= 'card-description'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aliquid quibusdam eos ad quae praesentium delectus aliquam nihil ipsam obcaecati tenetur, dicta repellat iusto est at veritatis odio. Molestiae, dolor?
+                At praesentium perspiciatis et aperiam vitae, dolorem ratione nobis adipisci commodi consequuntur architecto, dolores doloribus culpa dignissimos ipsum amet reiciendis voluptates nesciunt temporibus quae ducimus rerum deserunt dolore eum? Quae.</p>
+                <a href="#">Ver mas</a>
+
+            </div>
+
+        
+        </div>
+    )
+
+
+
+}
